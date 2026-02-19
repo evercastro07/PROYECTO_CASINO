cec86ae28b2df8c512089d0c5bbeac73a4bbe43c
Vela088 <adrian.vela@alumne.escolabalmes.com>
2026-02-19 18:55:41 +0100

    Add files via upload

---
+++ casino.js
@@ -0,0 +1,25 @@
+const CARTAS = [
+{nombre: "A", valor: "11"}
+{nombre: "2", valor: "2"}
+{nombre: "3", valor: "3"}
+{nombre: "4", valor: "4"}
+{nombre: "5", valor: "5"}
+{nombre: "6", valor: "6"}
+{nombre: "7", valor: "7"}
+{nombre: "8", valor: "8"}
+{nombre: "9", valor: "9"}
+{nombre: "10", valor: "10"}
+{nombre: "J", valor: "10"}
+{nombre: "Q", valor: "10"}
+{nombre: "K", valor: "10"}
+
+]
+const PALOS ["♠", "♥", "♦", "♣"]
+
+
+let baraja = []
+
+Function crearbaraja() {}
+baraja = []
+for (let palo of PALOS)
+    for (let carta of CARTAS)
